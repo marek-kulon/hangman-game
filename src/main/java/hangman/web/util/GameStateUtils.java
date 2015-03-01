d69fdf2c616fb44b9bf4fd62728f604ca935cae4
Marek Kulon <marek.kulon@gmail.com>
2015-03-01 22:33:05 +0000

    code refactoring

--- src/main/java/hangman/web/util/GameStateUtils.java
+++ src/main/java/hangman/web/util/GameStateUtils.java
@@ -4,16 +4,17 @@ import hangman.core.Game.GameStatus;
 import hangman.core.guess.Guess;
 import hangman.core.secret.Secret;
 import hangman.core.state.GameState;
-import org.apache.commons.lang3.Validate;
 
-import java.util.Set;
+import static java.util.stream.IntStream.range;
+import static org.apache.commons.lang3.Validate.isTrue;
+import static org.apache.commons.lang3.Validate.notNull;
 
 public class GameStateUtils {
 	
 	public static String getGuessedValue(GameState gameState, char notGuessedCharacterReplacement) {
-		Validate.notNull(gameState);
+		notNull(gameState);
 		// make sure it doesn't overlap with one of allowed secret characters
-		Validate.isTrue(!Secret.isValidSecretCharacter(notGuessedCharacterReplacement));
+		isTrue(!Secret.isValidSecretCharacter(notGuessedCharacterReplacement));
 		
 		// show secret if game is finished
 		if (!gameState.getGameStatus().equals(GameStatus.IN_PROGRESS)) {
@@ -21,14 +22,13 @@ public class GameStateUtils {
 		}
 		
 		// replace unknown characters with notGuessedCharacterReplacement
-		final Secret secret = gameState.getSecret();
-		final Set<Guess> guesses = gameState.getGuesses();
 		final StringBuilder sb = new StringBuilder();
-		for(int i=0; i<secret.getValue().length(); i++) {
-			char character = secret.getValue().charAt(i);
-			boolean useOriginal = character==Secret.SPACE_SEPARATOR || guesses.contains(Guess.of(character)); //check for space before creating new guess
-			sb.append(useOriginal ? character : notGuessedCharacterReplacement);
-		}
+        range(0, gameState.getSecret().getValue().length()).forEach( i -> {
+            char character = gameState.getSecret().getValue().charAt(i);
+            // display space/guessed character
+            boolean useOriginal = character==Secret.SPACE_SEPARATOR || gameState.getGuesses().contains(Guess.of(character));
+            sb.append(useOriginal ? character : notGuessedCharacterReplacement);
+        });
 		return sb.toString();
 	}
 
